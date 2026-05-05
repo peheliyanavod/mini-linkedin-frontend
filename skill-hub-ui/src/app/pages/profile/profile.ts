2e3afd59eb1bdd001b8e96dce92a717e141d4c79
Peheliya Dhanuka Navod <140178910+peheliyanavod@users.noreply.github.com>
2026-05-06 00:57:55 +0530

    make the profile page

--- skill-hub-ui/src/app/pages/profile/profile.ts
+++ skill-hub-ui/src/app/pages/profile/profile.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillService, Skill } from '../../services/skill.service';
 
 @Component({
   selector: 'app-profile',
+  standalone: true,
   imports: [],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile implements OnInit {
+  mySkills: Skill[] = [];
+  
+  // We are pretending you are logged in as User #1
+  currentUserId: number = 1; 
+
+  constructor(private skillService: SkillService) {}
+
+  ngOnInit() {
+    // Tell the waiter to only fetch skills for User #1!
+    this.skillService.getUserSkills(this.currentUserId).subscribe((data) => {
+      this.mySkills = data;
+    });
+  }
+}
--- skill-hub-ui/src/app/services/skill.service.ts
+++ skill-hub-ui/src/app/services/skill.service.ts
@@ -13,29 +13,33 @@ export interface Skill {
 })
 export class SkillService {
   // This is the endpoint we tested in Postman!
-  private apiUrl = 'http://localhost:8080/skills';
+  private apiUrl = 'http://localhost:8080';
 
   // We "hire" the waiter by injecting HttpClient into the constructor
   constructor(private http: HttpClient) { }
 
   // GET: Fetch all skills from Go
   getSkills(): Observable<Skill[]> {
-    return this.http.get<Skill[]>(this.apiUrl);
+    return this.http.get<Skill[]>(this.apiUrl + '/skills');
   }
 
   // POST: Send a new skill to Go
   addSkill(skill: Skill): Observable<Skill> {
-    return this.http.post<Skill>(this.apiUrl, skill);
+    return this.http.post<Skill>(this.apiUrl + '/skills', skill);
   }
 
   // DELETE: Remove a skill from Go
   deleteSkill(id: number): Observable<any> {
     // We are using the modern template literal syntax here!
-    return this.http.delete(`${this.apiUrl}?id=${id}`);
+    return this.http.delete(`${this.apiUrl}/skills?id=${id}`);
   }
 
   // PUT: Update an existing skill in Go
   updateSkill(id: number, skill: Skill): Observable<any> {
-    return this.http.put(`${this.apiUrl}?id=${id}`, skill);
+    return this.http.put(`${this.apiUrl}/skills?id=${id}`, skill);
+  }
+
+  getUserSkills(userId: number): Observable<any>{
+    return this.http.get(`${this.apiUrl}/users/skills?user_id=${userId}`);
   }
 }
