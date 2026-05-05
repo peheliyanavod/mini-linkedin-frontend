7545d91fef6d2f1ed60116e9a388eb5914ee18c3
Peheliya Dhanuka Navod <140178910+peheliyanavod@users.noreply.github.com>
2026-05-06 00:44:32 +0530

    make the home page

--- skill-hub-ui/src/app/pages/home/home.ts
+++ skill-hub-ui/src/app/pages/home/home.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillService, Skill } from '../../services/skill.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  skillsList: Skill[] = [];
+
+  // Bring in the waiter!
+  constructor(private skillService: SkillService) {}
+
+  // Fetch data when the page loads
+  ngOnInit() {
+    this.skillService.getSkills().subscribe((data) => {
+      this.skillsList = data;
+    });
+  }
+
+  addNewSkill(newSkillName: string) {
+    if (!newSkillName) return; 
+    const newSkillOrder: Skill = { skill_name: newSkillName };
+    this.skillService.addSkill(newSkillOrder).subscribe((savedSkill) => {
+      this.skillsList.push(savedSkill);
+    });
+  }
+
+  deleteSkill(id: number | undefined) {
+    if (!id) return;
+    this.skillService.deleteSkill(id).subscribe(() => {
+      this.skillsList = this.skillsList.filter(skill => skill.id !== id);
+    });
+  }
+
+  editSkill(skill: Skill) {
+    if (!skill.id) return;
+    const newName = prompt("Enter the updated skill name:", skill.skill_name);
+    if (newName && newName !== skill.skill_name) {
+      const updatedOrder = { skill_name: newName };
+      this.skillService.updateSkill(skill.id, updatedOrder).subscribe(() => {
+        skill.skill_name = newName;
+      });
+    }
+  }
+}
